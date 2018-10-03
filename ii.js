3696dfd85c4aadfd2880c3d4343e6720dfa3c8fe
rex <rex@gis.tw>
2018-10-03 11:15:22 +0800

    edit the ii.js

--- ii.js
+++ ii.js
@@ -25,22 +25,8 @@ map_ii_mvt.on('moveend', function (event) {
 });
 maps.push(map_ii_mvt);
 
-//var apiKey = 'UhguM5rA4BKa6p2UTZrG';
-//var style = 'https://maps.tilehosting.com/styles/basic/style.json?key=' + apiKey;
-//style = 'style.json';
-//fetch(style).then(function (response) {
-//    response.json().then(function (glStyle) {
-//        olms.applyStyle(layer, glStyle, 'composite').then(function () {
-//            map.addLayer(layer);
-//        });
-//    });
-//});
 
 /************ JVT *****************/
-//
-
-
-
 var group_ii = [];
 var ajax_setting = {
     type: "GET",
@@ -52,14 +38,28 @@ var ajax_setting = {
             var collection = data.collections[i];
 
             var name = collection.name;
-            //if (collection.name.toLowerCase() != 'agriculturesrf') continue;
             var l = new ol.layer.VectorTile({
                 title:collection.name,
                 source: new ol.source.VectorTile({
                     format: new ol.format.GeoJSON(),
                     url: 'https://services.interactive-instruments.de/vtp/daraa/collections/' + collection.name + '/items?f=json&count=100'//'
                 }),
-                style: style
+                style: function () {
+
+                    var layer = feature.get('layer');// there's no such property
+
+                    return new ol.style.Style({
+                        fill: new ol.style.Fill({
+                            color: '#80E87B',
+                            opacity: 0.6
+                        }),
+                        stroke: new ol.style.Stroke({
+                            color: '#AD60AA',
+                            width: 0.3
+                        })
+
+                    });
+                }
             });
             group_ii.push(l);
         }
@@ -82,34 +82,14 @@ function render_ii_jvtmap() {
         
         layers: [new ol.layer.Tile({
             source: new ol.source.OSM()
-        })/*, new ol.layer.VectorTile({
-            source: new ol.source.VectorTile({
-                format: new ol.format.GeoJSON(),
-                url: 'https://services.interactive-instruments.de/vtp/daraa/collections/agriculturesrf/items?f=json&count=1000'//'
-            }),
-            style:style
-        })*/,new ol.layer.Group({
+        }),new ol.layer.Group({
             title: 'Overlays',
             layers: group_ii
         })]
     });
     map_ii_jvt.addControl(new ol.control.LayerSwitcher());
-    //map_ii_jvt.on('pointermove', showInfo);
     map_ii_jvt.on('moveend', function (event) {
         moveend(event);
     });
     maps.push(map_ii_jvt);
 }
-//var group = new ol.layer.Group({
-//    title: 'Overlays',
-//    layers: [
-//        new ol.layer.VectorTile({
-//            title: 'ddd',
-//            source: new ol.source.VectorTile({
-//                format: new ol.format.MVT(),
-//                url: 'http://www.pvretano.com/cubewerx/cubeserv/default/wfs/3.0.0/foundation/collections/inwatera_1m/items?f=mvt&limit=20000&crs=epsg:3857&bbox=38.7527,-77.1593,39.0425,-76.8709'
-//            }),
-//            style: style
-//        })
-//    ]
-//})
